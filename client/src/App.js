48c841821262fa50ff17e865de276e26023e2695
Austin Osenga <osengaa10@icloud.com>
2021-08-13 10:27:01 -0400

    Doesnt work. Gonna have to revisit this.

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,44 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 // import ItemManagerContract from "./contracts/ItemManager.json";
 import FomoContract from "./contracts/FomoOE.json";
 import getWeb3 from "./getWeb3";
-import {Button, Badge, Alert} from 'react-bootstrap';
+import {Button, Container, Alert, Row, Col} from 'react-bootstrap';
 
 import "./App.css";
 
-class App extends Component {
-  state = { 
-    loaded: false, 
-    deposit: 0, 
-    withdraw: 0, 
-    total: 0,
-    totalKeyBalance: 0,
-    userKeyBalance: 0,
-    userKeyPurchaseAmount: 0,
-    keyPrice: 100
-   };
+function App() {
 
-  componentDidMount = async () => {
+  const [loaded, setLoaded] = useState(false);
+  const [deposit, setDeposit] = useState(0);
+  const [withdraw, setWithdraw] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [totalKeyBalance, setTotalKeyBalance] = useState(0);
+  const [userKeyBalance, setUserKeyBalance] = useState(0);
+  const [userKeyPurchaseAmount, setUserKeyPurchaseAmount] = useState(0);
+  const [keyPrice, setKeyPrice] = useState(100);
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  const init = async() => {
     try {
       // Get network provider and web3 instance.
-      this.web3 = await getWeb3();
+      let web3 = await getWeb3();
       console.log("this.web3: ");
       console.log(this.web3);
 
       // Use web3 to get the user's accounts.
-      this.accounts = await this.web3.eth.getAccounts();
+      const accounts = await web3.eth.getAccounts();
       console.log("this.accounts: ");
       console.log(this.accounts);
 
       // Get the contract instance.
-      this.networkId = await this.web3.eth.net.getId();
+      const networkId = await web3.eth.net.getId();
       console.log("this.networkId: ");
       console.log(this.networkId);
 
-      this.fomo = new this.web3.eth.Contract(
+      const fomo = new web3.eth.Contract(
         FomoContract.abi,
         FomoContract.networks[this.networkId] && FomoContract.networks[this.networkId].address,
       );
@@ -52,7 +55,7 @@ class App extends Component {
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
       // this.listenToPaymentEvent();
-      this.setState({ loaded: true });
+      setLoaded(true);
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -60,75 +63,61 @@ class App extends Component {
       );
       console.error(error);
     }
-    this.listenToDepositEvent();
-    this.listenToKeyPurchaseEvent();
+    listenToDepositEvent();
+    listenToKeyPurchaseEvent();
 
-    this.getContractBalance();
-    this.getUserKeyBalance();
-    this.getTotalKeyBalance();
-    this.getKeyPrice();
+    getContractBalance();
+    getUserKeyBalance();
+    getTotalKeyBalance();
+    getKeyPrice();
   };
-
-  // listenToPaymentEvent = () => {
-  //   let self = this;
-  //   this.fomo.events.SupplyChainStep().on("data", async function(evt) {
-  //     if(evt.returnValues._step === 1) {
-  //       let item = await self.itemManager.methods.items(evt.returnValues._itemIndex).call();
-  //       console.log(item);
-  //       alert("Item " + item._identifier + " was paid, deliver it now!");
-  //     };
-  //     console.log(evt);
-  //   });
-  // }
-  listenToDepositEvent = () => {
+  const listenToDepositEvent = () => {
     this.fomo.events.contractBalance().on("data", async (evt) => {
       // console.log(evt.returnValues._balanceReceived);
       // let totalBalance = await this
-      this.setState({ withdraw: evt.returnValues._balanceReceived }); 
-      console.log("this.state.withdraw:");   
-      console.log(this.state.withdraw);  
+      setWithdraw(evt.returnValues._balanceReceived);
+      console.log("withdraw:");   
+      console.log(withdraw);  
     });
 
   }
 
-  listenToKeyPurchaseEvent = () => {
+  const listenToKeyPurchaseEvent = () => {
     this.fomo.events.keysPurchased().on("data", async (evt) => {
       // console.log(evt.returnValues._balanceReceived);
       // let totalBalance = await this
-      this.setState({ userKeyBalance: evt.returnValues._userKeyBalance }); 
-      this.setState({ totalKeyBalance: evt.returnValues._totalKeys });
-      this.setState({ keyPrice: evt.returnValues._keyPrice })
-      console.log("this.state.userKeyBalance:");   
-      console.log(this.state.userKeyBalance);  
+      setUserKeyBalance(evt.returnValues._userKeyBalance);
+      setTotalKeyBalance(evt.returnValues._totalKeys);
+      setKeyPrice(evt.returnValues._keyPrice);
+      console.log("userKeyBalance:");   
+      console.log(userKeyBalance);  
     });
 
   }
 
-  handleInputChange = (event) => {
+  const handleInputChange = (event) => {
     const target = event.target;
     const value = target.type === "checkbox" ? target.checked : target.value;
     const name = target.name;
     console.log("event: ");
     console.log(event);
-    this.setState({
-      deposit: event.target.value
-    });
+    setDeposit(event.target.value);
   }
 
-  handleSubmit = async() => {
-    console.log("this.state.deposit:");
-    console.log(this.state.deposit);
+  const handleSubmit = async() => {
+    console.log("deposit:");
+    console.log(deposit);
     let result = await this.fomo.methods.receiveMoney().send({
       from: this.accounts[0], 
       to: this.CONTRACT_ADDRESS, 
-      value: this.web3.utils.toWei(this.state.deposit, "ether")
+      value: this.web3.utils.toWei(deposit, "ether")
     });
     console.log("result: ")
     console.log(result);
 
   }
 
-  handleWithdraw = async() => {
+  const handleWithdraw = async() => {
     let withdraw = await this.fomo.methods.withdrawMoney().send({
       from: this.accounts[0]
     })
@@ -138,54 +127,52 @@ class App extends Component {
   }
 
 
-  getContractBalance = async() => {
+  const getContractBalance = async() => {
     let balanceResult = await this.fomo.methods.getBalance().call()
-    this.setState({ withdraw: balanceResult });
+    setWithdraw(balanceResult);
     console.log("balanceResult");
     console.log(balanceResult);
   }
 
-  getUserKeyBalance = async() => {
+  const getUserKeyBalance = async() => {
     let getUserKeyBalanceResult = await this.fomo.methods.getUserKeyBalance().call()
-    this.setState({ userKeyBalance: getUserKeyBalanceResult });
+    setUserKeyBalance(getUserKeyBalanceResult);
     console.log("getUserKeyBalanceResult");
     console.log(getUserKeyBalanceResult);
   }
 
 
-  getTotalKeyBalance = async() => {
+  const getTotalKeyBalance = async() => {
     let getTotalKeyBalanceResult = await this.fomo.methods.getTotalKeyBalance().call()
-    this.setState({ totalKeyBalance: getTotalKeyBalanceResult });
+    setTotalKeyBalance(getTotalKeyBalanceResult);
     console.log("getTotalKeyBalanceResult");
     console.log(getTotalKeyBalanceResult);
   }
 
-  getKeyPrice = async() => {
+  const getKeyPrice = async() => {
     let getKeyPriceResult = await this.fomo.methods.getKeyPrice().call()
-    this.setState({ keyPrice: getKeyPriceResult });
+    setKeyPrice(getKeyPriceResult);
     console.log("getKeyPriceResult");
     console.log(getKeyPriceResult);
   }
 
 
-  handleKeyAmountChange = (event) => {
+  const handleKeyAmountChange = (event) => {
     const target = event.target;
     const value = target.type === "checkbox" ? target.checked : target.value;
     const name = target.name;
     console.log("event: ");
     console.log(event);
-    this.setState({
-      userKeyPurchaseAmount: event.target.value
-    });
+    setUserKeyPurchaseAmount(event.target.value);
   }
 
-  handlePurchaseKeys = async() => {
-    console.log("this.state.userKeyPurchaseAmount:");
-    console.log(this.state.userKeyPurchaseAmount);
-    let amountToPay = this.state.userKeyPurchaseAmount*this.state.keyPrice;
+  const handlePurchaseKeys = async() => {
+    console.log("userKeyPurchaseAmount:");
+    console.log(userKeyPurchaseAmount);
+    let amountToPay = userKeyPurchaseAmount*keyPrice;
     console.log("amountToPay:");
     console.log(amountToPay);
-    let result = await this.fomo.methods.purchaseKeys(this.state.userKeyPurchaseAmount).send({
+    let result = await this.fomo.methods.purchaseKeys(userKeyPurchaseAmount).send({
       from: this.accounts[0], 
       to: this.CONTRACT_ADDRESS, 
       value: amountToPay
@@ -195,34 +182,43 @@ class App extends Component {
 
   }
 
+
+
   
-  render() {
-    if (!this.state.loaded) {
-      return <div>Loading Web3, accounts, and contract...</div>;
-    }
+  // render() {
+  //   if (!loaded) {
+  //     return <div>Loading Web3, accounts, and contract...</div>;
+  //   }
     return (
       <div className="App">
+        <Container fluid>
         <h1>FomoOE</h1>
         <h2></h2>
-        <h2>Amount in Contract: {this.state.withdraw} wei</h2>
+        <h2>Amount in Contract: {withdraw} wei</h2>
         <div className="d-grid gap-2">
-        Amount to deposit: <input type="number" name="deposit" value={this.state.deposit} onChange={this.handleInputChange} />
-        <Button variant="danger" type="button" onClick={this.handleSubmit}>Deposit</Button>{' '}
-        <Button variant="success" size="lg" onClick={this.handleWithdraw}>Withdraw</Button>
-        {/* <Button variant="info" type="button" onClick={this.getContractBalance}>Get Contract Balance</Button> */}
+        Amount to deposit: <input type="number" name="deposit" value={deposit} onChange={handleInputChange} />
+        <Row className="mb-4">
+          <Col>
+            <Button variant="danger" size="lg" onClick={handleSubmit}>Deposit</Button>
+          </Col>
+          <Col>
+            <Button variant="success" size="lg" onClick={handleWithdraw}>Withdraw</Button>
+          </Col>
+        </Row>
         </div>
         <div className="d-grid gap-2">
-          <h2>Total Keys Bought: {this.state.totalKeyBalance}</h2>
-          <h3>Current Key Price: {this.state.keyPrice}</h3>
-          Amount of keys to buy: <input type="number" name="userKeyPurchaseAmount" value={this.state.userKeyPurchaseAmount} onChange={this.handleKeyAmountChange} />
-          <Button variant="info" type="button" onClick={this.handlePurchaseKeys}>Buy Keys!!!</Button>{' '}
+          <h2>Total Keys Bought: {totalKeyBalance}</h2>
+          <h3>Current Key Price: {keyPrice}</h3>
+          Amount of keys to buy: <input type="number" name="userKeyPurchaseAmount" value={userKeyPurchaseAmount} onChange={handleKeyAmountChange} />
+          <Button variant="info" type="button" onClick={handlePurchaseKeys}>Buy Keys!!!</Button>{' '}
           <Alert >
-              Keys owned: {this.state.userKeyBalance}
+              Keys owned: {userKeyBalance}
           </Alert>
         </div>
+        </Container>
       </div>
     );
-  }
+  
 }
 
 export default App;
